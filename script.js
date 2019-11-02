70068818e3d8e6ace32ae231ab270674bf293e9b
Endre Szabo <endre1988@freemail.hu>
2019-11-02 21:18:54 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+$("ul").on("click","li",function(){
+    /* if ($(this).css("color")==="rgb(128, 128, 128)"){
+        console.log("It is gray!");
+        $(this).css({color:"black",textDecoration:"none"});
+    }
+    else {
+    $(this).css({color:"gray",
+    textDecoration:"line-through"});
+    } */
+    $(this).toggleClass("completed");
+
+
+
+
+});
+
+$("ul").on("click","span",function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if (event.which===13){
+        let todoText = $(this).val();
+        $(this).val("");
+
+        $("ul").append("<li><span><i class='fa fa-trash'></i> </span>"+ todoText+ "</li>");
+    }
+
+})
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+});
