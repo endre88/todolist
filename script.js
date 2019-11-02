e26bdf83391a1b9312dae203f18e7d20fa1544b3
Endre Szabo <endre1988@freemail.hu>
2019-11-02 22:25:51 +0100

    Version 3

--- script.js
+++ script.js
@@ -8,10 +8,6 @@ $("ul").on("click","li",function(){
     textDecoration:"line-through"});
     } */
     $(this).toggleClass("completed");
-
-
-
-
 });
 
 $("ul").on("click","span",function(event){
@@ -22,7 +18,7 @@ $("ul").on("click","span",function(event){
 });
 
 $("input[type='text']").keypress(function(event){
-    if (event.which===13){
+    if ((event.which===13) && ($(this).val()!=="")){
         let todoText = $(this).val();
         $(this).val("");
 
@@ -30,7 +26,18 @@ $("input[type='text']").keypress(function(event){
     }
 
 })
+$(".fa-check").on("click",function(event){
+    if  ($("input[type='text']").val()!=="")  {
+        let todoText = $("input[type='text']").val();
+        $("input[type='text']").val("");
+        $("ul").append("<li><span><i class='fa fa-trash'></i> </span>"+ todoText+ "</li>");
+    }
+})
 
-$(".fa-plus").click(function(){
+$(this).addClass("fa-unlock")
+
+$("h1 i").click(function(){
     $("input[type='text']").fadeToggle();
+    $(".add").fadeToggle();    
+    $("h1 i").toggleClass("fa-lock fa-unlock"); //change icon with toggleClass
 });
